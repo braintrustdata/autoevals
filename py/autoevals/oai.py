a8b79e6874b7a907827547be798d7ff538a39baf
Ankur Goyal <ankrgyl@gmail.com>
2023-11-27 23:42:34 -0800

    Remove unecessary code

--- py/autoevals/oai.py
+++ py/autoevals/oai.py
@@ -1,38 +1,11 @@
 import asyncio
-import json
-import os
-import sqlite3
 import sys
 import textwrap
-import threading
 import time
 from pathlib import Path
 
 from .util import current_span
 
-_CACHE_DIR = None
-_CONN = None
-
-
-def set_cache_dir(path):
-    global _CACHE_DIR
-    _CACHE_DIR = path
-
-
-def open_cache():
-    global _CACHE_DIR, _CONN
-    if _CACHE_DIR is None:
-        _CACHE_DIR = Path.home() / ".cache" / "braintrust"
-
-    if _CONN is None:
-        oai_cache_path = Path(_CACHE_DIR) / "oai.sqlite"
-        os.makedirs(_CACHE_DIR, exist_ok=True)
-        _CONN = sqlite3.connect(oai_cache_path, check_same_thread=False)
-        _CONN.execute("CREATE TABLE IF NOT EXISTS cache (params text, response text)")
-    return _CONN
-
-
-CACHE_LOCK = threading.Lock()
 PROXY_URL = "https://braintrustproxy.com/v1"
 
 
@@ -101,20 +74,6 @@ def post_process_response(resp):
     else:
         # v1
         return resp.dict()
-
-
-def log_cached_response(params, resp):
-    with current_span().start_span(name="OpenAI Completion") as span:
-        messages = params.pop("messages", None)
-        span.log(
-            metrics={
-                "tokens": resp["usage"]["total_tokens"],
-                "prompt_tokens": resp["usage"]["prompt_tokens"],
-                "completion_tokens": resp["usage"]["completion_tokens"],
-            },
-            input=messages,
-            output=resp["choices"],
-        )
 
 
 def run_cached_request(api_key=None, base_url=None, **kwargs):

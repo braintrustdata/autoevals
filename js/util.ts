37337063db8be04bb482655d1ba98597bbe3a1eb
Manu Goyal <manu.goyal2013@gmail.com>
2023-12-23 17:11:53 -0800

    Align with updates to JS tracing API.
    Aligns with https://github.com/braintrustdata/braintrust-sdk/pull/80.

--- js/util.ts
+++ js/util.ts
@@ -6,18 +6,27 @@ declare global {
 
 // Signature taken from sdk/js/src/logger.ts::Span::traced.
 export function currentSpanTraced<R>(
-  name: string,
   callback: (spanLog: SpanLogFn) => R,
   args?: any
 ): R {
   if (globalThis.__inherited_braintrust_state) {
     const currentSpan =
       globalThis.__inherited_braintrust_state.currentSpan.getStore();
-    return currentSpan.traced(
-      name,
-      (span: any) => callback(span.log.bind(span)),
-      args
-    );
+    // Old versions of the API provide the name as the first positional
+    // argument. Otherwise, the name is provided as an optional keyword argument
+    // at the end.
+    if (currentSpan.traced.length === 2) {
+      return currentSpan.traced(
+        (span: any) => callback(span.log.bind(span)),
+        args
+      );
+    } else {
+      return currentSpan.traced(
+        args?.name ?? "subspan",
+        (span: any) => callback(span.log.bind(span)),
+        args
+      );
+    }
   } else {
     return callback(() => {});
   }

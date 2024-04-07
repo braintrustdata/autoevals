cde5b6644446df5a63f9d12aeeb26c3a1ef05f98
Ankur Goyal <ankrgyl@gmail.com>
2024-04-07 16:50:05 -0700

    Add RAGAS tests

---
+++ py/autoevals/test_ragas.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from pytest import approx
+
+from autoevals.ragas import *
+
+data = {
+    "input": "Can starred docs from different workspaces be accessed in one place?",
+    "output": "Yes, all starred docs, even from multiple different workspaces, will live in the My Shortcuts section.",
+    "expected": "Yes, all starred docs, even from multiple different workspaces, will live in the My Shortcuts section.",
+    "context": [
+        "Not all Coda docs are used in the same way. You'll inevitably have a few that you use every week, and some that you'll only use once. This is where starred docs can help you stay organized.\n\n\n\nStarring docs is a great way to mark docs of personal importance. After you star a doc, it will live in a section on your doc list called **[My Shortcuts](https://coda.io/shortcuts)**. All starred docs, even from multiple different workspaces, will live in this section.\n\n\n\nStarring docs only saves them to your personal My Shortcuts. It doesn\u2019t affect the view for others in your workspace. If you\u2019re wanting to shortcut docs not just for yourself but also for others in your team or workspace, you\u2019ll [use pinning](https://help.coda.io/en/articles/2865511-starred-pinned-docs) instead."
+    ],
+}
+
+
+def test_ragas_retrieval():
+    metrics = [
+        (ContextEntityRecall(), 0.69525),
+        (ContextRelevancy(), 0.7423),
+        (ContextRecall(), 1),
+        (ContextPrecision(), 1),
+    ]
+
+    for m, score in metrics:
+        assert m.eval(**data).score == approx(score, abs=1e-4)
+        assert asyncio.run(m.eval_async(**data)).score == approx(score, abs=1e-4)

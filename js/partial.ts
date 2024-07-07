c271cb8a537ad5e9dfa20f526fbd11f4550c8e7b
Ankur Goyal <ankrgyl@gmail.com>
2024-07-07 09:09:10 -0700

    Fix JS

--- js/partial.ts
+++ js/partial.ts
@@ -9,21 +9,18 @@ export function makePartial<Output, Extra>(
   fn: Scorer<Output, Extra>,
   name?: string
 ): ScorerWithPartial<Output, Extra> {
-  const fnCopy = fn.bind({});
-  const ret = {
-    ...fnCopy,
-    partial: (args: Partial<ScorerArgs<Output, Extra>>) => {
-      const newFn = (newArgs: ScorerArgs<Output, Extra>) =>
-        fnCopy({ ...args, ...newArgs });
-      if (name) {
-        Object.defineProperty(newFn, "name", {
-          value: name,
-          configurable: true,
-        });
-      }
-      return newFn;
-    },
-  } as ScorerWithPartial<Output, Extra>;
+  const ret: any = fn.bind({});
+  ret.partial = (args: Partial<ScorerArgs<Output, Extra>>) => {
+    const newFn = (newArgs: ScorerArgs<Output, Extra>) =>
+      ret({ ...args, ...newArgs });
+    if (name) {
+      Object.defineProperty(newFn, "name", {
+        value: name,
+        configurable: true,
+      });
+    }
+    return newFn;
+  };
   if (name) {
     Object.defineProperty(ret, "name", {
       value: name,

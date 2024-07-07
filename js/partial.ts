fb9d06faa77ec774e2fbd601808ee8e4a0dcd7e1
Ankur Goyal <ankrgyl@gmail.com>
2024-07-07 09:41:06 -0700

    Add some tests

--- js/partial.ts
+++ js/partial.ts
@@ -2,7 +2,13 @@ import { Scorer, ScorerArgs } from "@braintrust/core";
 
 export interface ScorerWithPartial<Output, Extra>
   extends Scorer<Output, Extra> {
-  partial: (args: Partial<ScorerArgs<Output, Extra>>) => Scorer<Output, Extra>;
+  partial: <T extends Partial<ScorerArgs<Output, Extra>>>(
+    args: T
+  ) => Scorer<
+    Output,
+    // Mark all of the provided parameters as optional in the new scorer
+    Omit<Extra, keyof T> & { [K in keyof T]?: T[K] | undefined }
+  >;
 }
 
 export function makePartial<Output, Extra>(

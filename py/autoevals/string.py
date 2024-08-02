2240b7ef03fdc78b43bbba81a60702c085cd7b04
Austin Moehle <frozenvapor100@gmail.com>
2024-08-02 16:24:28 -0700

    Normalize value to string

--- py/autoevals/string.py
+++ py/autoevals/string.py
@@ -59,6 +59,7 @@ class EmbeddingSimilarity(ScorerWithPartial):
             self.extra_args["base_url"] = base_url
 
     async def _a_embed(self, value):
+        value = normalize_value(value, maybe_object=False)
         with self._CACHE_LOCK:
             if value in self._CACHE:
                 return self._CACHE[value]
@@ -71,6 +72,7 @@ class EmbeddingSimilarity(ScorerWithPartial):
         return result
 
     def _embed(self, value):
+        value = normalize_value(value, maybe_object=False)
         with self._CACHE_LOCK:
             if value in self._CACHE:
                 return self._CACHE[value]

6ef91bea079ad26884cfbb72c603e0d0623f5f51
mongodben <90647379+mongodben@users.noreply.github.com>
2024-08-26 17:00:21 -0400

    Clean up test

--- js/oai.test.ts
+++ js/oai.test.ts
@@ -1,19 +1,11 @@
 import { buildOpenAIClient } from "./oai";
 
-describe("OAI", () => {
+describe.skip("OAI", () => {
   test("should use Azure OpenAI", async () => {
-    /* TODO: I wasn't sure how to propagate these
-    variables from the environment to the test.
-    I just left placeholders here for now.
-    You can plug in your own valid Azure OpenAI credentials
-    to make sure it works.
-
-    Also there weren't any tests for the `oai.ts` file
-    previously, so not sure if we want to add this now.
-
-    Defer to the Braintrust team on how to handle integration
-    of Azure OpenAI support.
-    */
+    /*
+     * You can plug in your own valid Azure OpenAI info
+     * to make sure it works.
+     */
     const client = buildOpenAIClient({
       azureOpenAi: {
         apiKey: "<some api key>",
@@ -28,7 +20,7 @@ describe("OAI", () => {
         },
       ],
     } = await client.chat.completions.create({
-      model: "gpt-4o-mini", // or whatever your relevant Azure OpenAI deployment name is
+      model: "<Azure OpenAI LLM deployment name>",
       messages: [{ role: "system", content: "Hello" }],
     });
     expect(content).toBeTruthy();
